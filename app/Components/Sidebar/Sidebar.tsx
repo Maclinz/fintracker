e75bbc7771a0c63035437031d977c3abbd9bf0e0
maclinz <tafadzwamucheke@gmail.com>
2024-02-24 17:08:26 +0000

    feat: active nav

--- app/Components/Sidebar/Sidebar.tsx
+++ app/Components/Sidebar/Sidebar.tsx
@@ -3,7 +3,8 @@ import nav from "@/app/utils/nav";
 import { useTheme } from "@/context/themeContext";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import { usePathname } from "next/navigation";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 function Sidebar() {
@@ -11,6 +12,8 @@ function Sidebar() {
 
   const help = nav.slice(5, 7);
   const settings = nav.slice(7, 9);
+
+  const pathname = usePathname();
 
   return (
     <SidebarStyled theme={theme}>
@@ -24,10 +27,13 @@ function Sidebar() {
             <li key={index} className="mt-1">
               <Link
                 href={item.link}
-                className="px-2 py-2 flex items-center gap-2"
+                className={`relative px-2 py-2 flex items-center gap-2
+                  ${pathname === item.link && "active"}
+                `}
               >
                 {item.icon}
                 {item.title}
+                <span className="dot"> </span>
               </Link>
             </li>
           );
@@ -39,10 +45,13 @@ function Sidebar() {
             <li key={index} className="mt-1">
               <Link
                 href={item.link}
-                className="px-2 py-2 flex items-center gap-2"
+                className={`relative px-2 py-2 flex items-center gap-2 ${
+                  pathname === item.link && "active"
+                }`}
               >
                 {item.icon}
                 {item.title}
+                <span className="dot"></span>
               </Link>
             </li>
           );
@@ -54,7 +63,9 @@ function Sidebar() {
             <li key={index} className="mt-1">
               <Link
                 href={item.link}
-                className="px-2 py-2 flex items-center gap-2"
+                className={`px-2 py-2 flex items-center gap-2 ${
+                  pathname === item.link && "active"
+                }`}
               >
                 {item.icon}
                 {item.title}
@@ -78,6 +89,38 @@ const SidebarStyled = styled.nav`
 
   display: flex;
   flex-direction: column;
+
+  ul li a {
+    transition: all 0.3s ease-in-out;
+    border-radius: 0.5rem;
+
+    &:hover {
+      background-color: ${({ theme }) => theme.colorNeutral};
+      border-radius: 0.5rem;
+      transition: all 0.3s ease-in-out;
+      color: ${({ theme }) => theme.colorPurple};
+    }
+  }
+
+  .active {
+    background-color: ${({ theme }) => theme.colorNeutral3};
+    border-radius: 0.5rem;
+    color: ${({ theme }) => theme.colorPurple};
+
+    font-weight: 500;
+
+    .dot {
+      position: absolute;
+      right: 0.5rem;
+      top: 50%;
+      transform: translateY(-50%);
+
+      width: 0.3rem;
+      height: 0.3rem;
+      background-color: ${({ theme }) => theme.colorPurple};
+      border-radius: 50%;
+    }
+  }
 `;
 
 export default Sidebar;
--- app/utils/themes.js
+++ app/utils/themes.js
@@ -3,9 +3,12 @@ const themes = [
     name: "light",
     colorNeutral: "#DFE3F8",
     colorNeutral2: "#F7F8FE",
+    colorNeutral3: "#ECEAFB",
     colorBlack: "#000000",
     colorWhite: "#FFFFFF",
     colorWhiteBg: "#FFFFFF",
+    colorPurple: "#7D5BE5",
+    colorPurple2: "#9982E9",
   },
   {
     name: "dark",
